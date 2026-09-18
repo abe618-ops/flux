dee5b4e55498b787f127d5835e0debfc60a74481
abe618-ops <abe618@gmail.com>
2026-09-18 20:01:19 +0800

    jshare: add app build config

---
+++ minimal-share/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.flux.jshare"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.flux.jshare"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
